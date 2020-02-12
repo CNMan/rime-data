2f9d4f75284a1aa3f25894093a8eb6e864a0ef52
CNMan <nklijl@mail.nankai.edu.cn>
2020-02-12 23:36:52 +0800

    注音、倉頡、朙月拼音、袖珍簡化字拼音、五筆畫、地球拼音

---
+++ ReadMe.md
@@ -0,0 +1,39 @@
+# 新世紀五筆字型 for Rime
+
+## [Rime 簡明教程](https://06wb.github.io/Tutorials/rime.html)
+
+## 交流羣組
+
+QQ羣：38021162、857031696
+
+[Telegram](https://telegram.org/) 羣组 [@wbinput](https://t.me/wbinput)
+
+## 檔案說明
+
+```
+* 通規詞庫版新世紀五筆(CNMan)：tygfhzb_wubi06.dict.yaml、tygfhzb_wubi06.schema.yaml
+* 通規詞庫版新世紀五筆[出簡不出全](CNMan)：tygfhzb_cjbcq_wubi06.dict.yaml、tygfhzb_cjbcq_wubi06.schema.yaml
+* 通規單字版新世紀五筆(CNMan)：tygfhzb_dz_wubi06.dict.yaml、tygfhzb_dz_wubi06.schema.yaml
+* GB18030單字版新世紀五筆(CNMan)：gb18030-2000_dz_wubi06.dict.yaml、gb18030-2000_dz_wubi06.schema.yaml
+* 超集單字版新世紀五筆(CNMan)：unicodecjk_wubi06.dict.yaml、unicodecjk_wubi06.schema.yaml
+* 86五筆（極爽詞庫4.3）：jishuang_wubi86.dict.yaml、jishuang_wubi86.schema.yaml
+* 98五筆（昱瓊詞庫2.1）：yuqiong_wubi98.dict.yaml、yuqiong_wubi98.schema.yaml
+* 98五筆（至善詞庫2.0.191211）：zhishan_wubi98.dict.yaml、zhishan_wubi98.schema.yaml
+```
+
+另有Rime官方維護或預裝的：
+
+```
+注音：https://github.com/rime/rime-bopomofo
+倉頡：https://github.com/rime/rime-cangjie
+粵語拼音：https://github.com/rime/rime-cantonese
+雙拼：https://github.com/rime/rime-double-pinyin
+繪文字：https://github.com/rime/rime-emoji
+八股文：https://github.com/rime/rime-essay
+粵拼：https://github.com/rime/rime-jyutping
+朙月拼音：https://github.com/rime/rime-luna-pinyin
+袖珍簡化字拼音：https://github.com/rime/rime-pinyin-simp
+五筆畫：https://github.com/rime/rime-stroke
+地球拼音：https://github.com/rime/rime-terra-pinyin
+86五筆（極爽詞庫6.0）：https://github.com/rime/rime-wubi
+```
---
+++ rime_sync.cmd
@@ -0,0 +1,95 @@
+mode con cols=500 lines=500
+cd /d "E:\GitHub\OpenCC\" && git pull
+cd /d "E:\GitHub\rime\DictTool\" && git pull
+cd /d "E:\GitHub\rime\lotem\rime-guhuwubi\" && git pull
+cd /d "E:\GitHub\rime\lotem\rime-linguistic-wubi\" && git pull
+cd /d "E:\GitHub\rime\lotem\rime-wubi98\" && git pull
+cd /d "E:\GitHub\rime\lotem\rime-zhengma\" && git pull
+cd /d "E:\GitHub\rime\rime\rime-bopomofo\" && git pull
+cd /d "E:\GitHub\rime\rime\rime-cangjie\" && git pull
+cd /d "E:\GitHub\rime\rime\rime-cantonese\" && git pull
+cd /d "E:\GitHub\rime\rime\rime-double-pinyin\" && git pull
+cd /d "E:\GitHub\rime\rime\rime-emoji\" && git pull
+cd /d "E:\GitHub\rime\rime\rime-essay\" && git pull
+cd /d "E:\GitHub\rime\rime\rime-jyutping\" && git pull
+cd /d "E:\GitHub\rime\rime\rime-luna-pinyin\" && git pull
+cd /d "E:\GitHub\rime\rime\rime-pinyin-simp\" && git pull
+cd /d "E:\GitHub\rime\rime\rime-stroke\" && git pull
+cd /d "E:\GitHub\rime\rime\rime-terra-pinyin\" && git pull
+cd /d "E:\GitHub\rime\rime\rime-wubi\" && git pull
+cd /d "E:\GitHub\rime\rime-configuration\bosconb\Rime_sync\" && git pull
+cd /d "E:\GitHub\rime\rime-configuration\byujiang\wubi06_rime\" && git pull
+cd /d "E:\GitHub\rime\rime-configuration\cmal\rime-conf\" && git pull
+cd /d "E:\GitHub\rime\rime-configuration\g178253\rime-wubi06\" && git pull
+cd /d "E:\GitHub\rime\rime-configuration\GuoBinyong\wubixinshiji\" && git pull
+cd /d "E:\GitHub\rime\rime-configuration\hz-lang\wb06\" && git pull
+cd /d "E:\GitHub\rime\rime-configuration\jiqiangwubi\SuperWubiSchema\" && git pull
+cd /d "E:\GitHub\rime\rime-configuration\kktt007\rime-fcitx-rime\" && git pull
+cd /d "E:\GitHub\rime\rime-configuration\lswqzhang\RIME-wubi\" && git pull
+cd /d "E:\GitHub\rime\rime-configuration\outsinre\fcitx-rime\" && git pull
+cd /d "E:\GitHub\rime\rime-configuration\Pioneer-Linzi\xinshijiwubi\" && git pull
+cd /d "E:\GitHub\rime\rime-configuration\rtransformation\rime-opencc_emoji_symbols\" && git pull
+cd /d "E:\GitHub\rime\rime-configuration\tigren\rime_configs\" && git pull
+cd /d "E:\GitHub\rime\rime-configuration\youanan\rime\" && git pull
+taskkill /IM WeaselServer.exe /F
+del "E:\VMware\Rime\Weasel\7z.dll"
+del "E:\VMware\Rime\Weasel\7z.exe"
+del "E:\VMware\Rime\Weasel\7-zip-license.txt"
+del "E:\VMware\Rime\Weasel\COPYING-curl.txt"
+del "E:\VMware\Rime\Weasel\curl.exe"
+del "E:\VMware\Rime\Weasel\curl-ca-bundle.crt"
+del "E:\VMware\Rime\Weasel\LICENSE.txt"
+del "E:\VMware\Rime\Weasel\README.txt"
+del "E:\VMware\Rime\Weasel\rime-install.bat"
+del "E:\VMware\Rime\Weasel\rime-install-config.bat"
+del "E:\VMware\Rime\Weasel\uninstall.exe"
+del "E:\VMware\Rime\Weasel\data\bopomofo.schema.yaml"
+del "E:\VMware\Rime\Weasel\data\bopomofo_express.schema.yaml"
+del "E:\VMware\Rime\Weasel\data\bopomofo_tw.schema.yaml"
+del "E:\VMware\Rime\Weasel\data\cangjie5.dict.yaml"
+del "E:\VMware\Rime\Weasel\data\cangjie5.schema.yaml"
+del "E:\VMware\Rime\Weasel\data\cangjie5_express.schema.yaml"
+del "E:\VMware\Rime\Weasel\data\essay.txt"
+del "E:\VMware\Rime\Weasel\data\luna_pinyin.dict.yaml"
+del "E:\VMware\Rime\Weasel\data\luna_pinyin.schema.yaml"
+del "E:\VMware\Rime\Weasel\data\luna_pinyin_fluency.schema.yaml"
+del "E:\VMware\Rime\Weasel\data\luna_pinyin_simp.schema.yaml"
+del "E:\VMware\Rime\Weasel\data\luna_pinyin_tw.schema.yaml"
+del "E:\VMware\Rime\Weasel\data\luna_quanpin.schema.yaml"
+del "E:\VMware\Rime\Weasel\data\pinyin_simp.dict.yaml"
+del "E:\VMware\Rime\Weasel\data\pinyin_simp.schema.yaml"
+del "E:\VMware\Rime\Weasel\data\stroke.dict.yaml"
+del "E:\VMware\Rime\Weasel\data\stroke.schema.yaml"
+del "E:\VMware\Rime\Weasel\data\terra_pinyin.dict.yaml"
+del "E:\VMware\Rime\Weasel\data\terra_pinyin.schema.yaml"
+copy "E:\GitHub\rime\rime\rime-bopomofo\bopomofo.schema.yaml" "E:\VMware\Rime\data\bopomofo.schema.yaml" /y
+copy "E:\GitHub\rime\rime\rime-bopomofo\bopomofo_express.schema.yaml" "E:\VMware\Rime\data\bopomofo_express.schema.yaml" /y
+copy "E:\GitHub\rime\rime\rime-bopomofo\bopomofo_tw.schema.yaml" "E:\VMware\Rime\data\bopomofo_tw.schema.yaml" /y
+copy "E:\GitHub\rime\rime\rime-cangjie\cangjie5.dict.yaml" "E:\VMware\Rime\data\cangjie5.dict.yaml" /y
+copy "E:\GitHub\rime\rime\rime-cangjie\cangjie5.schema.yaml" "E:\VMware\Rime\data\cangjie5.schema.yaml" /y
+copy "E:\GitHub\rime\rime\rime-cangjie\cangjie5_express.schema.yaml" "E:\VMware\Rime\data\cangjie5_express.schema.yaml" /y
+copy "E:\GitHub\rime\rime\rime-essay\essay.txt" "E:\VMware\Rime\data\essay.txt" /y
+copy "E:\GitHub\rime\rime\rime-luna-pinyin\luna_pinyin.dict.yaml" "E:\VMware\Rime\data\luna_pinyin.dict.yaml" /y
+copy "E:\GitHub\rime\rime\rime-luna-pinyin\luna_pinyin.schema.yaml" "E:\VMware\Rime\data\luna_pinyin.schema.yaml" /y
+copy "E:\GitHub\rime\rime\rime-luna-pinyin\luna_pinyin_fluency.schema.yaml" "E:\VMware\Rime\data\luna_pinyin_fluency.schema.yaml" /y
+copy "E:\GitHub\rime\rime\rime-luna-pinyin\luna_pinyin_simp.schema.yaml" "E:\VMware\Rime\data\luna_pinyin_simp.schema.yaml" /y
+copy "E:\GitHub\rime\rime\rime-luna-pinyin\luna_pinyin_tw.schema.yaml" "E:\VMware\Rime\data\luna_pinyin_tw.schema.yaml" /y
+copy "E:\GitHub\rime\rime\rime-luna-pinyin\luna_quanpin.schema.yaml" "E:\VMware\Rime\data\luna_quanpin.schema.yaml" /y
+copy "E:\GitHub\rime\rime\rime-pinyin-simp\pinyin_simp.dict.yaml" "E:\VMware\Rime\data\pinyin_simp.dict.yaml" /y
+copy "E:\GitHub\rime\rime\rime-pinyin-simp\pinyin_simp.schema.yaml" "E:\VMware\Rime\data\pinyin_simp.schema.yaml" /y
+copy "E:\GitHub\rime\rime\rime-stroke\stroke.dict.yaml" "E:\VMware\Rime\data\stroke.dict.yaml" /y
+copy "E:\GitHub\rime\rime\rime-stroke\stroke.schema.yaml" "E:\VMware\Rime\data\stroke.schema.yaml" /y
+copy "E:\GitHub\rime\rime\rime-terra-pinyin\terra_pinyin.dict.yaml" "E:\VMware\Rime\data\terra_pinyin.dict.yaml" /y
+copy "E:\GitHub\rime\rime\rime-terra-pinyin\terra_pinyin.schema.yaml" "E:\VMware\Rime\data\terra_pinyin.schema.yaml" /y
+rd /s /q "E:\VMware\Rime\data\build\"
+rd /s /q "E:\VMware\Rime\data\luna_pinyin.userdb\"
+cd /d "E:\VMware\Rime\Weasel\"
+start /wait WeaselDeployer /deploy
+nircmd exec2 hide "E:\VMware\Rime\Weasel\" "E:\VMware\Rime\Weasel\WeaselServer.exe"
+cd /d E:\VMware\Rime\data\
+sed -i 's/aaaaaaaaaaaaaaaaaaaaaaaaaaaa/bbbbbbbbbbbbbbbbbbbbbbbbbbbb/g' *.yaml
+git diff
+git add .
+git commit -m "Sync Pre-Installed Official Schemas"
+git push
+pause
